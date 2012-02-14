cd9a08fa8af5ca34dc2e1afec784f9c33afa490f
Valerio Gheri <valerio.gheri@gmail.com>
2012-02-14 17:09:01 +0100

    Added capability to send data to the server using XMLHttpRequest2 object
    Signed-off-by: Valerio Gheri <valerio.gheri@gmail.com>

--- DesktopDragAndDrop/Controllers/HomeController.cs
+++ DesktopDragAndDrop/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DesktopDragAndDrop.Models;
 
 namespace DesktopDragAndDrop.Controllers
 {
@@ -15,9 +16,49 @@ namespace DesktopDragAndDrop.Controllers
             return View();
         }
 
+        [HttpPost]        
+        public ActionResult Upload()
+        {
+            UploadedFile file = RetrieveFileFromRequest();
+
+            return RedirectToAction("Index");
+                      
+        }
+
         public ActionResult About()
         {
             return View();
+        }
+
+        private UploadedFile RetrieveFileFromRequest()
+        {
+            string filename = null;
+            string fileType = null;
+            byte[] fileContents = null;
+
+            if (Request.Files.Count > 0)
+            { //they're uploading the old way
+                var file = Request.Files[0];
+                fileContents = new byte[file.ContentLength];
+                fileType = file.ContentType;
+                filename = file.FileName;
+            }
+            else if (Request.ContentLength > 0)
+            {
+                // Using FileAPI the content is in Request.InputStream!!!!
+                fileContents = new byte[Request.ContentLength];
+                Request.InputStream.Read(fileContents, 0, Request.ContentLength);
+                filename = Request.Headers["X-File-Name"];
+                fileType = Request.Headers["X-File-Type"];
+            }
+
+            return new UploadedFile()
+            {
+                Filename = filename,
+                ContentType = fileType,
+                FileSize = fileContents != null ? fileContents.Length : 0,
+                Contents = fileContents
+            };
         }
     }
 }
